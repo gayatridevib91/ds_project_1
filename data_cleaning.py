39f2df9ab25b10026188fef91a2412b1ba7ff5dc
gayatri B <gayatridevib91@gmail.com>
2021-09-27 15:50:43 +0100

    added EDA process

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,63 +1,65 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep 20 11:55:12 2021
+Created on Sat Apr  4 17:27:06 2020
 
-@author: Gayatri Devi
+@author: Ken
 """
 
+import pandas as pd 
 
-import pandas as pan
-from datetime import date 
-date=date.today()
-yr = int(date.year)
-df = pan.read_csv("glassdoor_jobs.csv")
-df = df[df['Salary Estimate']!= '-1']
-#to find out columns that are hourly paid
-df['hourly']=df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0)
-#to find out columns that has emp provided sal in them
-df['Emp_Provided_Salary:']=df['Salary Estimate'].apply(lambda x: 1 if 'employer provided salary:' in x.lower() else 0)
+df = pd.read_csv('glassdoor_jobs.csv')
 
-Salary = df['Salary Estimate'].apply(lambda x: x.split('(')[0])
-salary_minus_k_and_dollar = Salary.apply(lambda x: x.replace('K','').replace('$',''))
-Salary_numbers = salary_minus_k_and_dollar.apply(lambda x: x.lower().replace('per hour','').replace('employer provided salary:',''))
+#salary parsing 
 
-df['min_salary']=Salary_numbers.apply(lambda x: int(x.split('-')[0]))
+df['hourly'] = df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0)
+df['employer_provided'] = df['Salary Estimate'].apply(lambda x: 1 if 'employer provided salary:' in x.lower() else 0)
 
-df['max_salary']=Salary_numbers.apply(lambda x: int(x.split('-')[1]))
-df['avg_salary']= (df.min_salary+df.max_salary)/2
-#having only company name as text in the field
-df['companyName_text']=df.apply(lambda x: x['Company Name'] if x['Rating']<0 else x['Company Name'][:-3],axis=1 )
-df['job_state']=df['Location'].apply(lambda x: x.split(',')[1])
+df = df[df['Salary Estimate'] != '-1']
+salary = df['Salary Estimate'].apply(lambda x: x.split('(')[0])
+minus_Kd = salary.apply(lambda x: x.replace('K','').replace('$',''))
+
+min_hr = minus_Kd.apply(lambda x: x.lower().replace('per hour','').replace('employer provided salary:',''))
+
+df['min_salary'] = min_hr.apply(lambda x: int(x.split('-')[0]))
+df['max_salary'] = min_hr.apply(lambda x: int(x.split('-')[1]))
+df['avg_salary'] = (df.min_salary+df.max_salary)/2
+
+#Company name text only
+df['company_txt'] = df.apply(lambda x: x['Company Name'] if x['Rating'] <0 else x['Company Name'][:-3], axis = 1)
+
+#state field 
+df['job_state'] = df['Location'].apply(lambda x: x.split(',')[1])
 df.job_state.value_counts()
-df['same_state'] = df.apply(lambda x: 1 if x.Location == x.Headquarters else 0 ,axis=1)
 
-df['age_Cmpny']=df.Founded.apply(lambda x: x if x<1 else yr - x)
-#parsing job description
-#for tools like python,R,spark,AWS,excel 
+df['same_state'] = df.apply(lambda x: 1 if x.Location == x.Headquarters else 0, axis = 1)
 
-#fro python
+#age of company 
+df['age'] = df.Founded.apply(lambda x: x if x <1 else 2020 - x)
+
+#parsing of job description (python, etc.)
+
+#python
 df['python_yn'] = df['Job Description'].apply(lambda x: 1 if 'python' in x.lower() else 0)
-df.python_yn.value_counts()
-
-#for R
-df['R_yn'] = df['Job Description'].apply(lambda x: 1 if 'r studio' or 'r_studio' in x.lower() else 0)
+ 
+#r studio 
+df['R_yn'] = df['Job Description'].apply(lambda x: 1 if 'r studio' in x.lower() or 'r-studio' in x.lower() else 0)
 df.R_yn.value_counts()
 
-#for spark
-df['spark_yn'] = df['Job Description'].apply(lambda x: 1 if 'spark' in x.lower() else 0)
-df.spark_yn.value_counts()
+#spark 
+df['spark'] = df['Job Description'].apply(lambda x: 1 if 'spark' in x.lower() else 0)
+df.spark.value_counts()
 
-#for AWS
-df['aws_yn'] = df['Job Description'].apply(lambda x: 1 if 'aws' in x.lower() else 0)
-df.aws_yn.value_counts()
+#aws 
+df['aws'] = df['Job Description'].apply(lambda x: 1 if 'aws' in x.lower() else 0)
+df.aws.value_counts()
 
-#for excel
-df['excel_yn'] = df['Job Description'].apply(lambda x: 1 if 'spark' in x.lower() else 0)
-df.excel_yn.value_counts()
+#excel
+df['excel'] = df['Job Description'].apply(lambda x: 1 if 'excel' in x.lower() else 0)
+df.excel.value_counts()
 
-#dropping unnmaed columns
 df.columns
 
-df_out=df.drop('Unnamed: 0',axis =1)
+df_out = df.drop(['Unnamed: 0'], axis =1)
 
-d#f_out.to_csv('Salary_Data_cleansed.csv',index=False)
+df_out.to_csv('salary_data_cleaned.csv',index = False)
+
